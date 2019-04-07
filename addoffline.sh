a59596cfbf47ca5735686bce875b418df2e1603f
caubequay00 <caubequay00@gmail.com>
2019-04-07 10:59:44 +0700

    Update add device

--- addoffline.sh
+++ addoffline.sh
@@ -2,7 +2,12 @@
 
 while true
 do
-  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline")
+  if [ -z "$2" ]
+  then
+	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"device\":\"$1\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline")
+  else
+	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online")
+  fi
   if [ $STATUS -eq 201 ]; then
     echo "Got 201! All done!"
     break
--- stream.sh
+++ stream.sh
@@ -3,6 +3,7 @@
 # Diffusion youtube avec ffmpeg
 FILE=./$(basename $0).pid
 #trap 'curl "https://api.telegram.org/bot840166611:AAGho_96r93saYmKMyx2FHxLFGMI1DQR_ro/sendMessage?chat_id=531864213&text=$1"' EXIT
+./addoffline.sh $DEVICE
 trap 'bash addoffline.sh $1 $2 $3' EXIT
 #trap ' status_code=$(curl -k -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[\"$1|$2|$3\"]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline" --write-out %{http_code};  )' EXIT
 # Configurer youtube avec une résolution 720p. La vidéo n'est pas scalée.
@@ -16,5 +17,6 @@ else
     ./ffmpeg -re -stream_loop -1 -i "input.mp4" -filter_complex setpts=PTS/1 -vcodec libx264 -pix_fmt yuv420p -b:v 1000k -acodec libmp3lame -b:a 128k -ar 44100 -threads 0 -preset superfast -maxrate 1000k -bufsize 2500k -f flv -s 1280x720 "$1"  
     echo $! > $FILE
 fi
+
 #rtmp://a.rtmp.youtube.com/live2/./ffmpeg -re -stream_loop -1 -i "input.mp4" -vcodec libx264 -pix_fmt yuv420p -b:v 1000k -preset veryfast -maxrate 1000k -bufsize 2500k -vf "format=yuv420p" -g 60 -acodec libmp3lame -b:a 198k -ar 44100 -metadata title="" -metadata artist="" -metadata album_artist="" -metadata album="" -metadata date="" -metadata track="" -metadata genre="" -metadata publisher="" -metadata encoded_by="" -metadata copyright="" -metadata composer="" -metadata performer="" -metadata TIT1="" -metadata TIT3="" -metadata disc="" -metadata TKEY="" -metadata TBPM="" -metadata language="eng" -metadata encoder="" -f flv "rtmp://a.rtmp.youtube.com/live2/$1"
 
