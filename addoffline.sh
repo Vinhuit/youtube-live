7fe53e984b31f618d953ab42a948af82ce55e82e
Vinh Huynh <caubequay00@gmail.com>
2019-04-07 11:29:29 +0700

    Update addoffline.sh

--- addoffline.sh
+++ addoffline.sh
@@ -4,7 +4,7 @@ while true
 do
   if [ -z "$2" ]
   then
-	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"device\":\"$1\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline")
+	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"device\":\"$DEVICE\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline")
   else
 	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online")
   fi
