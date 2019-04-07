06ad0cdedee18dbbb89f3812326bfa71b65f8690
Vinh Huynh <caubequay00@gmail.com>
2019-04-07 11:48:45 +0700

    Update addoffline.sh

--- addoffline.sh
+++ addoffline.sh
@@ -4,6 +4,7 @@ do
   if [ -z "$2" ]
   then
 	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"device\":\"$DEVICE\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline")
+	  node server.js &
   else
 	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online")
   fi
