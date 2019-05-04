daf681ae314d039ca9b8078ca9268ff0a29ae43f
caubequay00 <caubequay00@gmail.com>
2019-05-04 22:45:28 +0700

    update database

--- addoffline.sh
+++ addoffline.sh
@@ -7,11 +7,11 @@ do
 	fi
   fi
   if [ -z "$2" ]; then
-    	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH --data "[{\"device\":\"$DEVICE\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/offline/$1")
+    	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH --data "[{\"device\":\"$DEVICE\"}]" "http://xjsonserver01.herokuapp.com/offline/$1")
 	    else
 	    	  times=$(grep -oP 'time=...........' log.txt | tail -1| cut -f2 -d '=' )
 		  	  echo $times
-			  	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\",\"times\":\"$times\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online/$4")
+			  	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\",\"times\":\"$times\"}]" "http://xjsonserver01.herokuapp.com//online/$4")
 				  #	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online/$4")
 				    fi
 				      if [ $STATUS -eq 201 ] || [ $STATUS -eq 200 ]; then
@@ -19,7 +19,7 @@ do
 					      break
 				      elif [ $STATUS -eq 404 ]; then
 				        echo "Got $STATUS :( Not done yet..."
-			  	        STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\",\"times\":\"$times\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online/")
+			  	        STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\",\"times\":\"$times\"}]" "http://xjsonserver01.herokuapp.com/online/")
 				       else
 					  echo "Got $STATUS :( Not done yet..."
 				        fi
