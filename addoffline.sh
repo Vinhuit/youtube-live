38dc3e8652b4e104d56b905dd767324666474e82
caubequay00 <caubequay00@gmail.com>
2019-05-04 23:53:13 +0700

    update stream.sh

--- addoffline.sh
+++ addoffline.sh
@@ -12,6 +12,12 @@ do
 	    	  times=$(grep -oP 'time=...........' log.txt | tail -1| cut -f2 -d '=' )
 		  	  echo $times
 			  	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$DEVICE\",\"times\":\"$times\"}" "http://xjsonserver01.herokuapp.com//online/$NUM")
+				  sleep 1
+			  	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$DEVICE\",\"times\":\"$times\"}" "http://xjsonserver01.herokuapp.com//online/$NUM")
+				  sleep 1
+			  	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$DEVICE\",\"times\":\"$times\"}" "http://xjsonserver01.herokuapp.com//online/$NUM")
+
+
 				  #	  STATUS=$(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "[{\"key\":\"$1\",\"link\":\"$2\",\"device\":\"$3\"}]" "http://myjsonserver-winiss.1d35.starter-us-east-1.openshiftapps.com/online/$4")
 				    fi
 				      if [ $STATUS -eq 201 ] || [ $STATUS -eq 200 ]; then
